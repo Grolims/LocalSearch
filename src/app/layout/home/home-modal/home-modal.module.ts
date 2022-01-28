af99db7dbeb79c07c83bf229f9c9886970b89f79
Martin Mertenat <41158878+Grolims@users.noreply.github.com>
2022-01-28 20:33:43 +0100

    bug modal ngif

--- src/app/layout/home/home-modal/home-modal.module.ts
+++ src/app/layout/home/home-modal/home-modal.module.ts
@@ -11,6 +11,7 @@ import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { SalePointDetailPageModule } from '../sale-point-detail/sale-point-detail.module';
 import { ItemDetailPageModule } from '../item-detail/item-detail.module';
+import { ProfilePageModule } from '../profile/profile.module';
 
 @NgModule({
   imports: [
@@ -21,7 +22,9 @@ import { ItemDetailPageModule } from '../item-detail/item-detail.module';
     LeafletModule,
     Ng2SearchPipeModule,
     SalePointDetailPageModule,
-    ItemDetailPageModule
+    ItemDetailPageModule,
+    ProfilePageModule,
+
   ],
   declarations: [HomeModalPage]
 })
