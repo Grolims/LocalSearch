e64aecda22edd43959b3d8b17856d0a2315faa83
Martin Mertenat <41158878+Grolims@users.noreply.github.com>
2022-01-29 17:26:43 +0100

    add salepoints on items detail

--- src/app/layout/home/item-detail/item-detail.page.ts
+++ src/app/layout/home/item-detail/item-detail.page.ts
@@ -3,6 +3,9 @@ import { Router } from '@angular/router';
 import { ItemResponseValue } from 'src/app/models/item';
 import { NavparamService } from 'src/app/navparam.service';
 import { Itemservice } from 'src/app/services/item.service';
+import { SalepointMarkerResponseValue } from 'src/app/models/salepointmarker';
+import { Salepointservice } from 'src/app/services/salepoint.service';
+import { SalepointResponseValue } from 'src/app/models/salepoint';
 
 @Component({
   selector: 'app-item-detail',
@@ -15,13 +18,34 @@ export class ItemDetailPage implements OnInit {
 
   data:any = 0;
   items:ItemResponseValue[] = [];
+  salepoints:SalepointMarkerResponseValue[] = [];
 
-  constructor(private navParamService:NavparamService, private router: Router, private itemService: Itemservice) {
+
+  constructor(private navParamService:NavparamService, private salepointService: Salepointservice, private router: Router, private itemService: Itemservice) {
 
     this.data = this.navParamService.getNavData();
+
+    this.addSalepoint();
+    console.log(this.salepoints);
   }
 
- 
+  addSalepoint() {
+
+    this.salepointService.getSalepointIDs().subscribe(salepoint => {
+      salepoint.data.forEach(element => {
+        if (element._id == this.data.salepointId)
+        {
+          this.salepoints.push(element);
+
+        }
+
+      });
+
+
+    });
+  }
+
+
 
   ngOnInit() {
   }
