748214b2c82997a2b60451f9c32b84e23605c38a
Martin Mertenat <41158878+Grolims@users.noreply.github.com>
2022-01-29 14:54:47 +0100

    Home != home

--- src/app/layout/home/home-modal/home-modal.module.ts
+++ src/app/layout/home/home-modal/home-modal.module.ts
@@ -9,10 +9,11 @@ import { HomeModalPageRoutingModule } from './home-modal-routing.module';
 import { HomeModalPage } from './home-modal.page';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
-import { SalePointDetailPageModule } from 'src/app/Layout/Home/sale-point-detail/sale-point-detail.module';
-import { ItemDetailPageModule } from 'src/app/Layout/home/item-detail/item-detail.module';
-import { ProfilePageModule } from 'src/app/layout/home/profile/profile.module';
-import { CreateSalepointPageModule } from 'src/app/layout/home/profile/create-salepoint/create-salepoint.module';
+import { CreateSalepointPageModule } from '../profile/create-salepoint/create-salepoint.module';
+import { ItemDetailPageModule } from '../item-detail/item-detail.module';
+import { ProfilePageModule } from '../profile/profile.module';
+import { SalePointDetailPageModule } from '../sale-point-detail/sale-point-detail.module';
+
 
 
 @NgModule({
